b12ad3af31dca1e59b8e78d839dc985dcfc0821b
MatheusNLima <muitomato@gmail.com>
2022-09-17 14:30:43 -0300

    teste

---
+++ java/hello_world.java
@@ -0,0 +1,7 @@
+package java;
+public class hello_world{
+    public static void main (String[] args) {
+        System.out.println("hello world");
+        System.out.println("my first java program");
+    }
+}
